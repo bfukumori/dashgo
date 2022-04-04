c2bf401b2ae062cc54344a84e6c8c0b128203048
Bruno Fukumori <brunofukumori@gmail.com>
2022-04-04 19:32:17 -0300

    fix page name to SignIn

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,7 @@ import { Button, Flex, Stack } from "@chakra-ui/react";
 import { Input } from "../components/Form/Input";
 
 
-export default function Home() {
+export default function SignIn() {
   return (
     <Flex w="100vw" h="100vh" align="center" justify="center">
       <Flex as="form" w="100%" maxW={360} bg="gray.800" p="8" borderRadius={8} flexDir="column">
